96ec180ec8da9b11c5cb8789a20c138409b39844
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-13 00:06:33 +0530

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'  // Create a separate CSS file for styling
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Rishi Jain</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> {/* Align items to the right */}
+                        <li className="nav-item">
+                                <Link className="nav-link" to="/education">Education</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/work">Work Experience</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/projects">Projects</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
