4d4aaafb9554357aa291384cacf79512152e9abb
Rishi-Jain2602 <rj1016743@gmail.com>
2025-01-13 01:08:42 +0530

    fix Navbar menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,12 @@ export default function Navbar() {
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">Rishi Jain</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <i className="fas fa-bars"></i> 
                     </button>
+
+                    
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> {/* Align items to the right */}
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0"> 
                         <li className="nav-item">
                                 <Link className="nav-link" to="/education">Education</Link>
                             </li>
